69f73105843bb76ef3d3e0641354475d21ce1848
Sikta Das Adhikari <73495177+Sikta-Das-Adhikari@users.noreply.github.com>
2023-01-23 09:42:06 -0500

    Update and rename BayesKAT.R to BayesKAT_Original.R

---
+++ Code/BayesKAT_Original.R
@@ -0,0 +1,14 @@
+#Code written by: Sikta Das Adhikari
+## MCMC based bayesian optimum kernel selection and testing.
+
+
+rm(list=ls())
+###################
+# load packages ###
+###################
+library(ttutils)
+library(methods)
+library(psych)
+library(EQL)
+library(mvtnorm)
+library(Matrix)
