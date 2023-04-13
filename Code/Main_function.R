ce1ae0ed335bc57f9bff6d0198e25773fd4c8456
Sikta Das Adhikari <73495177+Sikta-Das-Adhikari@users.noreply.github.com>
2023-04-13 16:03:04 -0400

    Update Main_function.R

--- Code/Main_function.R
+++ Code/Main_function.R
@@ -1,7 +1,7 @@
 #this is the main function BayesKAT which can be used to call any of the strategies: BayesKAT-MCMC or BayesKAT-MAP
 
 rm(list=ls())
-source("/Users/siktadasadhikari/Desktop/Test/Sub_functions.R") 
+source("../Sub_functions.R") 
 
 main_function<-function(inputAddress_y,inputAddress_X,inputAddress_Z,prior_H1=0.5,output_address,Strategy="BayesKAT-MAP"){
   if(Strategy=="BayesKAT-MAP"){
